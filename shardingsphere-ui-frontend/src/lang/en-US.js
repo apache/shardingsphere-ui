a0bf48953afe54cfe498fe33f8a34a503a496f78
Eason <eason.fengys@gmail.com>
2020-07-03 21:36:45 +0800

    Add metadata info shown in ShardingSphere-UI

--- shardingsphere-ui-frontend/src/lang/en-US.js
+++ shardingsphere-ui-frontend/src/lang/en-US.js
@@ -184,7 +184,7 @@ export default {
       name: 'Please enter the name of the schema',
       ruleConfig: 'Please enter the rule config of the schema',
       dataSourceConfig: 'Please enter the data source config of the schema',
-      metadataConfig: 'Please enter the data source config of the schema'
+      metadataConfig: 'Please enter the metadata config of the schema'
     },
     radioBtn: {
       schema: 'Schema',
